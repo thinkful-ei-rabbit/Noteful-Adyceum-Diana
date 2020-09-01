5477d9d7a6f0b5b00e23f41e7c0205719e047639
Adyceum A. Magna Ccarri <61805757+i-MCcarri@users.noreply.github.com>
2020-09-01 10:59:47 -0700

    added boilerplate examples

--- noteful-diana-ady/src/App.js
+++ noteful-diana-ady/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+      notes: [],
+      folders: []
+  };
+
+  //Boilerpolate Sample:
+//     return (
+//         <div className="app">
+//             // Exact route - will only match /
+//             <Route exact path="/" component={HomePage} />
+//             // Inexact route - will match /settings, /settings/profile
+//             // and /settings/email
+//             <Route path="/settings" component={SettingsNavigation} />
+//             <Route
+//                 exact
+//                 path="/settings/profile"
+//                 component={ProfileSettings}
+//             />
+//             // If you want to pass props to the component, use the render prop
+//             <Route
+//                 exact
+//                 path="/settings/email"
+//                 render={() => <EmailSettings email="jturner@thinkful.com" />}
+//             />
+//         </div>
+//     );
+// }
 
 export default App;
